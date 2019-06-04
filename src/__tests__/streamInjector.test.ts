ab673aa21bb68292b223103957e475bed811c0b9
Alexander Wong <alex@udia.ca>
2019-06-04 12:14:58 -0600

    Working injecting node stream transformer

---
+++ src/__tests__/streamInjector.test.ts
@@ -0,0 +1,304 @@
+import { Readable } from "stream";
+import { StreamInjector } from "../streamInjector";
+
+describe("string replaceValue", () => {
+  describe("non-boundary", () => {
+    it("should inject string using search string", done => {
+      expect.assertions(1);
+      const injectTransformer = new StreamInjector("consectetur", "replaced");
+
+      const readStream = new Readable({ read: () => undefined });
+      readStream.push("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+      readStream.push(null);
+      const testStream = readStream.pipe(injectTransformer);
+
+      const holder: string[] = [];
+      testStream.on("data", (chunk: Buffer) => { holder.push(chunk.toString()); });
+      testStream.on("end", () => {
+        expect(holder.join("")).toEqual("Lorem ipsum dolor sit amet, replaced adipiscing elit.");
+        done();
+      });
+    });
+
+    it("should inject string using search regexp", done => {
+      expect.assertions(1);
+      const injectTransformer = new StreamInjector(/efficitur/, "replaced");
+
+      const readStream = new Readable({ read: () => undefined });
+      readStream.push("Mauris pretium mauris id leo efficitur bibendum.");
+      readStream.push(null);
+      const testStream = readStream.pipe(injectTransformer);
+
+      const holder: string[] = [];
+      testStream.on("data", (chunk: Buffer) => { holder.push(chunk.toString()); });
+      testStream.on("end", () => {
+        expect(holder.join("")).toEqual("Mauris pretium mauris id leo replaced bibendum.");
+        done();
+      });
+    });
+
+    it("should inject multiple strings using search string", done => {
+      expect.assertions(1);
+      const injectTransformer = new StreamInjector("consectetur", "replaced");
+
+      const readStream = new Readable({ read: () => undefined });
+      readStream.push("Lorem consectetur dolor sit amet, consectetur adipiscing elit.");
+      readStream.push(null);
+      const testStream = readStream.pipe(injectTransformer);
+
+      const holder: string[] = [];
+      testStream.on("data", (chunk: Buffer) => { holder.push(chunk.toString()); });
+      testStream.on("end", () => {
+        expect(holder.join("")).toEqual("Lorem replaced dolor sit amet, replaced adipiscing elit.");
+        done();
+      });
+    });
+
+    it("should inject multiple strings using search regexp", done => {
+      expect.assertions(1);
+      const injectTransformer = new StreamInjector(/efficitur/, "replaced");
+
+      const readStream = new Readable({ read: () => undefined });
+      readStream.push("Mauris pretium efficitur id leo efficitur bibendum.");
+      readStream.push(null);
+      const testStream = readStream.pipe(injectTransformer);
+
+      const holder: string[] = [];
+      testStream.on("data", (chunk: Buffer) => { holder.push(chunk.toString()); });
+      testStream.on("end", () => {
+        expect(holder.join("")).toEqual("Mauris pretium replaced id leo replaced bibendum.");
+        done();
+      });
+    });
+
+    it("should preserve order on no match", done => {
+      expect.assertions(1);
+      const injectTransformer = new StreamInjector(/efficitur/, "no match");
+
+      const readStream = new Readable({ read: () => undefined });
+      readStream.push("Vivamus ");
+      readStream.push("lobortis purus ");
+      readStream.push("in lectus egestas maximus.");
+      readStream.push(null);
+      const testStream = readStream.pipe(injectTransformer);
+
+      const holder: string[] = [];
+      testStream.on("data", (chunk: Buffer) => { holder.push(chunk.toString()); });
+      testStream.on("end", () => {
+        expect(holder.join("")).toEqual("Vivamus lobortis purus in lectus egestas maximus.");
+        done();
+      });
+
+    });
+  });
+
+  describe("boundary", () => {
+    it("should inject string using search string (boundary)", done => {
+      expect.assertions(1);
+      const injectTransformer = new StreamInjector("consectetur", "replaced");
+
+      const readStream = new Readable({ read: () => undefined });
+      readStream.push("Lorem ipsum dolor sit amet, conse");
+      readStream.push("ctetur adipiscing elit.");
+      readStream.push(null);
+      const testStream = readStream.pipe(injectTransformer);
+
+      const holder: string[] = [];
+      testStream.on("data", (chunk: Buffer) => { holder.push(chunk.toString()); });
+      testStream.on("end", () => {
+        expect(holder.join("")).toEqual("Lorem ipsum dolor sit amet, replaced adipiscing elit.");
+        done();
+      });
+    });
+
+    it("should inject string using search regexp (boundary)", done => {
+      expect.assertions(1);
+      const injectTransformer = new StreamInjector(/efficitur/, "replaced");
+
+      const readStream = new Readable({ read: () => undefined });
+      readStream.push("Mauris pretium mauris id leo effi");
+      readStream.push("citur bibendum.");
+      readStream.push(null);
+      const testStream = readStream.pipe(injectTransformer);
+
+      const holder: string[] = [];
+      testStream.on("data", (chunk: Buffer) => { holder.push(chunk.toString()); });
+      testStream.on("end", () => {
+        expect(holder.join("")).toEqual("Mauris pretium mauris id leo replaced bibendum.");
+        done();
+      });
+    });
+
+    it("should inject multiple strings using search string (double boundary)", done => {
+      expect.assertions(1);
+      const injectTransformer = new StreamInjector("consectetur", "replaced");
+
+      const readStream = new Readable({ read: () => undefined });
+      readStream.push("Lorem consectetur");
+      readStream.push(" dolor sit amet, conse");
+      readStream.push("ctetur adipiscing elit.");
+      readStream.push(null);
+      const testStream = readStream.pipe(injectTransformer);
+
+      const holder: string[] = [];
+      testStream.on("data", (chunk: Buffer) => { holder.push(chunk.toString()); });
+      testStream.on("end", () => {
+        expect(holder.join("")).toEqual("Lorem replaced dolor sit amet, replaced adipiscing elit.");
+        done();
+      });
+    });
+
+    it("should inject multiple strings using search regexp (double boundary)", done => {
+      expect.assertions(1);
+      const injectTransformer = new StreamInjector(/efficitur/, "replaced");
+
+      const readStream = new Readable({ read: () => undefined });
+      readStream.push("Mauris eff");
+      readStream.push("icitu");
+      readStream.push("r mauris id leo effi");
+      readStream.push("citur bibendum.");
+      readStream.push(null);
+      const testStream = readStream.pipe(injectTransformer);
+
+      const holder: string[] = [];
+      testStream.on("data", (chunk: Buffer) => { holder.push(chunk.toString()); });
+      testStream.on("end", () => {
+        expect(holder.join("")).toEqual("Mauris replaced mauris id leo replaced bibendum.");
+        done();
+      });
+    });
+
+  });
+});
+
+describe("stream replaceValue", () => {
+  describe("non-boundary", () => {
+    it("should inject stream using search string", done => {
+      expect.assertions(1);
+      const replaceStream = new Readable({ read: () => undefined });
+      replaceStream.push("replaced");
+      replaceStream.push(null);
+      const injectTransformer = new StreamInjector("consectetur", replaceStream);
+
+      const readStream = new Readable({ read: () => undefined });
+      readStream.push("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+      readStream.push(null);
+      const testStream = readStream.pipe(injectTransformer);
+
+      const holder: string[] = [];
+      testStream.on("data", (chunk: Buffer) => { holder.push(chunk.toString()); });
+      testStream.on("end", () => {
+        expect(holder.join("")).toEqual("Lorem ipsum dolor sit amet, replaced adipiscing elit.");
+        done();
+      });
+    });
+
+    it("should inject stream using search regexp", done => {
+      expect.assertions(1);
+      const replaceStream = new Readable({ read: () => undefined });
+      replaceStream.push("replaced");
+      replaceStream.push(null);
+      const injectTransformer = new StreamInjector(/efficitur/, replaceStream);
+
+      const readStream = new Readable({ read: () => undefined });
+      readStream.push("Mauris pretium mauris id leo efficitur bibendum.");
+      readStream.push(null);
+      const testStream = readStream.pipe(injectTransformer);
+
+      const holder: string[] = [];
+      testStream.on("data", (chunk: Buffer) => { holder.push(chunk.toString()); });
+      testStream.on("end", () => {
+        expect(holder.join("")).toEqual("Mauris pretium mauris id leo replaced bibendum.");
+        done();
+      });
+    });
+  });
+
+  describe("boundary", () => {
+    it("should inject stream using search string (boundary)", done => {
+      expect.assertions(1);
+      const replaceStream = new Readable({ read: () => undefined });
+      replaceStream.push("replaced");
+      replaceStream.push(null);
+      const injectTransformer = new StreamInjector("consectetur", replaceStream);
+
+      const readStream = new Readable({ read: () => undefined });
+      readStream.push("Lorem ipsum dolor sit amet, conse");
+      readStream.push("ctetur adipiscing elit.");
+      readStream.push(null);
+      const testStream = readStream.pipe(injectTransformer);
+
+      const holder: string[] = [];
+      testStream.on("data", (chunk: Buffer) => { holder.push(chunk.toString()); });
+      testStream.on("end", () => {
+        expect(holder.join("")).toEqual("Lorem ipsum dolor sit amet, replaced adipiscing elit.");
+        done();
+      });
+    });
+
+    it("should inject stream using search regexp (boundary)", done => {
+      expect.assertions(1);
+      const replaceStream = new Readable({ read: () => undefined });
+      replaceStream.push("replaced");
+      replaceStream.push(null);
+      const injectTransformer = new StreamInjector(/efficitur/, replaceStream);
+
+      const readStream = new Readable({ read: () => undefined });
+      readStream.push("Mauris pretium mauris id leo effi");
+      readStream.push("citur bibendum.");
+      readStream.push(null);
+      const testStream = readStream.pipe(injectTransformer);
+
+      const holder: string[] = [];
+      testStream.on("data", (chunk: Buffer) => { holder.push(chunk.toString()); });
+      testStream.on("end", () => {
+        expect(holder.join("")).toEqual("Mauris pretium mauris id leo replaced bibendum.");
+        done();
+      });
+    });
+
+    it("should inject stream once using search string (double boundary)", done => {
+      expect.assertions(1);
+      const replaceStream = new Readable({ read: () => undefined });
+      replaceStream.push("replaced");
+      replaceStream.push(null);
+      const injectTransformer = new StreamInjector("consectetur", replaceStream);
+
+      const readStream = new Readable({ read: () => undefined });
+      readStream.push("Lorem ipsum consectetur sit amet, conse");
+      readStream.push("ctetur adipiscing elit.");
+      readStream.push(null);
+      const testStream = readStream.pipe(injectTransformer);
+
+      const holder: string[] = [];
+      testStream.on("data", (chunk: Buffer) => { holder.push(chunk.toString()); });
+      testStream.on("end", () => {
+        // Only the first instance of the search value should have been replaced
+        expect(holder.join("")).toEqual("Lorem ipsum replaced sit amet, consectetur adipiscing elit.");
+        done();
+      });
+    });
+
+    it("should inject stream once using search regexp (double boundary)", done => {
+      expect.assertions(1);
+      const replaceStream = new Readable({ read: () => undefined });
+      replaceStream.push("replaced");
+      replaceStream.push(null);
+      const injectTransformer = new StreamInjector(/efficitur/, replaceStream);
+
+      const readStream = new Readable({ read: () => undefined });
+      readStream.push("Mauris pretium efficitur id leo effi");
+      readStream.push("citur bibendum.");
+      readStream.push(null);
+      const testStream = readStream.pipe(injectTransformer);
+
+      const holder: string[] = [];
+      testStream.on("data", (chunk: Buffer) => { holder.push(chunk.toString()); });
+      testStream.on("end", () => {
+        expect(holder.join("")).toEqual("Mauris pretium replaced id leo efficitur bibendum.");
+        done();
+      });
+    });
+  });
+
+});
---
+++ src/streamInjector.ts
@@ -0,0 +1,94 @@
+import { Stream, Transform, TransformCallback } from "stream";
+
+// tslint:disable-next-line: interface-name
+interface StreamInjectorOptions {
+  encoding?: BufferEncoding;
+}
+
+export class StreamInjector extends Transform {
+  private chunksStack: string[] = [];
+  private searchValue: string | RegExp;
+  private replaceValue: string | Stream;
+  private injectOptions: StreamInjectorOptions;
+  private overlapWindow: number;
+  private streamReplaced: boolean;
+
+  constructor(searchValue: string | RegExp, replaceValue: string | Stream, injectOptions?: StreamInjectorOptions) {
+    super();
+    this.searchValue = searchValue;
+    this.replaceValue = replaceValue;
+    this.injectOptions = injectOptions || {};
+
+    // maximum number of characters to check for search value overlap
+    this.overlapWindow = searchValue.toString().length;
+    // set an instance flag for already replaced stream
+    this.streamReplaced = false;
+  }
+
+  public _transform(chunk: any, _: string, callback: TransformCallback) {
+    const { encoding } = this.injectOptions;
+    let streamCallbackInterrupted = false;
+
+    if (!Buffer.isBuffer(chunk)) {
+      throw new TypeError(`Uninjectable type: ${typeof chunk}`);
+    }
+
+    let chunkStr = chunk.toString(encoding);
+    let data: Buffer | undefined;
+    while (this.chunksStack.length) {
+      chunkStr = this.chunksStack.pop() + chunkStr;
+    }
+
+    const matchIndex = chunkStr.search(this.searchValue);
+    if (matchIndex >= 0) {
+      const chunkFragments = chunkStr.split(this.searchValue);
+      let carryOver = true;
+      if (typeof this.replaceValue === "string") {
+        const lastFragment = chunkFragments[chunkFragments.length - 1];
+        const replChunk = chunkFragments.join(this.replaceValue);
+        const cap = replChunk.lastIndexOf(lastFragment);
+        data = Buffer.from(replChunk.slice(0, cap), encoding);
+        this.chunksStack.push(lastFragment);
+        carryOver = false;
+      } else if (!this.streamReplaced) {
+        data = Buffer.from(chunkFragments[0], encoding);
+        this.push(data);
+        streamCallbackInterrupted = true;
+        this.streamReplaced = true;
+        this.replaceValue.on("data", (replChunk) =>  this.push(replChunk));
+        this.replaceValue.on("end", () => callback(null));
+      } else {
+        data = Buffer.from(chunkStr, encoding);
+        carryOver = false;
+      }
+
+      if (carryOver && chunkFragments.length > 1 && chunkFragments[1] !== "") {
+        const carryOverIndex = chunkStr.indexOf(chunkFragments[1], matchIndex);
+        this.chunksStack.push(chunkStr.slice(carryOverIndex));
+      }
+    } else {
+      const pivot = (chunkStr.length - this.overlapWindow);
+      if (pivot > 0) {
+        const sendFragment = chunkStr.slice(0, pivot);
+        data = Buffer.from(sendFragment, encoding);
+        this.chunksStack.push(chunkStr.slice(pivot));
+      } else {
+        this.chunksStack.push(chunkStr);
+      }
+    }
+
+    if (! streamCallbackInterrupted) {
+      callback(null, data);
+    }
+  }
+
+  public _flush(callback: TransformCallback) {
+    while (this.chunksStack.length) {
+      const currentChunkString = this.chunksStack.pop();
+      if (currentChunkString) {
+        this.push(Buffer.from(currentChunkString, this.injectOptions.encoding));
+      }
+    }
+    callback(null);
+  }
+}
